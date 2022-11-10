deb14afdba03d6238bfa8c4371245f87f55ac659
jinnurain-jihan <zinnurainzihan171@gmail.com>
2022-11-10 10:41:30 +0600

    javascript

---
+++ app.js
@@ -0,0 +1,31 @@
+//Number
+let aNumber=10;
+let anotherNumber=30.5;
+console.log(anotherNumber);
+console.log(typeof aNumber);
+console.log(typeof anotherNumber);
+//string
+let text1="i wamt to say something";
+let text2="i love to eat pizza";
+let text3="200";
+console.log(text1);
+console.log(text2);
+console.log(text3);
+console.log(typeof text1);
+console.log(typeof text2);
+console.log(typeof text3);
+//Boolean
+let isTrue=true;
+let isFalse=false;
+console.log(isTrue);
+console.log(isFalse);
+console.log(typeof isTrue);
+console.log(typeof isFalse);
+//undefined
+let name;
+console.log(name);
+console.log(typeof name);
+//null
+let ban=null;
+console.log(ban);
+console.log(typeof ban);
